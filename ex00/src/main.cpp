528e98d7e8eb8ad3611b6b72d3d5d9c6c8d368ee
tblaase <tblaase@student.42heilbronn.de>
2022-03-08 15:56:37 +0100

    ex00 finished, check again before handing in :)

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,25 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:46:09 by tblaase           #+#    #+#             */
-/*   Updated: 2022/02/23 13:46:10 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/03/08 15:43:56 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+				argv[i][j] = (char)toupper(argv[i][j]);
+			std::cout << argv[i] << " ";
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+}
