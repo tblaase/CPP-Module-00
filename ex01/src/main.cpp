d1383b2efd0d4c57c2014fc88db79e8b9546a760
tblaase <tblaase@student.42heilbronn.de>
2022-03-14 17:51:12 +0100

    now works with empty command as intended

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:46:13 by tblaase           #+#    #+#             */
-/*   Updated: 2022/03/14 14:50:24 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/03/14 17:39:52 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,12 @@ int main(void)
 	std::cout << "\033[33m$>\033[0m";
 	while (run && std::getline(std::cin, command))
 	{
-		if (command.at(0) == (char)4)
-			command.clear();
-		if (command.compare("ADD") == 0)
+		if (std::cin.eof() == true)
+		{
+			std::cout << "You Pressed ^D. Exiting phonebook now." << std::endl;
+			exit(0);
+		}
+		else if (command.compare("ADD") == 0)
 			PhoneBook.set_information();
 		else if (command.compare("SEARCH") == 0)
 			PhoneBook.get_information();
